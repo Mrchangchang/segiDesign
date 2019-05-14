02ec4e7b3cf7a2ed53c327a1ff5d9600b8981810
MrChang <78172670@qq.com>
2019-05-15 01:43:41 +0800

    编写preview组件

--- src/preview/DesignPreview.vue
+++ src/preview/DesignPreview.vue
@@ -1,13 +1,31 @@
-<script>
-import { find, some, defaultTo, isFunction, get } from 'lodash';
+<template>
+  <Container>
+    <div
+      :class="[`${prefix}-design-preview`, className]"
+      :style="get(
+              settings,
+              'previewBackground',
+              DEFAULT_BACKGROUND
+            )"
+    >
+      <div :class="`${prefix}-design__disabled-mask`" v-if="disabled">
+      </div>
+      <Droppable></Droppable>
+    </div>
+  </Container>
+</template>
 
-import DesignPreviewItem from './DesignPreviewItem';
-import DesignPreviewController from './DesignPreviewController';
-import DesignEditorItem from '../editor/DesignEditorItem';
-import DesignEditorAddComponent from '../editor/DesignEditorAddComponent';
-import { isExpectedDesignType } from '../utils/design-type';
-import { DND_PREVIEW_CONTROLLER, DEFAULT_BACKGROUND } from './constants';
-import { ADD_COMPONENT_OVERLAY_POSITION } from '../constants';
+<script>
+import { Container, Draggable } from "vue-smooth-dnd";
+import { find, some, defaultTo, isFunction, get } from "lodash";
+
+import DesignPreviewItem from "./DesignPreviewItem";
+import DesignPreviewController from "./DesignPreviewController";
+import DesignEditorItem from "../editor/DesignEditorItem";
+import DesignEditorAddComponent from "../editor/DesignEditorAddComponent";
+import { isExpectedDesignType } from "../utils/design-type";
+import { DND_PREVIEW_CONTROLLER, DEFAULT_BACKGROUND } from "./constants";
+import { ADD_COMPONENT_OVERLAY_POSITION } from "../constants";
 
 /**
  * DesignPreview 和 config 组件是相互关联的
@@ -15,11 +33,52 @@ import { ADD_COMPONENT_OVERLAY_POSITION } from '../constants';
  * 这个组件里的一些 props 是需要 config 组件提供的
  */
 export default {
-    name: 'DesignPreview',
-    props: {
-        className: {
-            type: String
-        }
+  name: "DesignPreview",
+  components: { Container, Draggable },
+  props: {
+    className: String,
+    prefix: {
+      type: String,
+      default: "segi"
+    },
+    design: Object,
+    value: Array,
+    settings: Object,
+    onSettingsChange: Function,
+    footer: Object,
+    appendableComponents: {
+      type: Array,
+      default: () => []
+    },
+    showAddComponentOverlay: Boolean,
+    addComponentOverlayPosition: Number,
+    selectedUUID: String,
+    getUUIDFromValue: Function,
+    onSelect: Function,
+    onAdd: Function, // 处理添加按钮的回调函数
+    onAddComponent: Function, // 添加新组件
+    onDelete: Function,
+    onEdit: Function,
+    onMove: Function,
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    componentInstanceCount: Object, // 每个组件的示例数量
+    background: {
+      type: String,
+      default: "#f9f9f9"
     }
-}
+  },
+  data() {
+    return {
+      previewItems: {},
+      editorItems: {},
+      editors: {}
+    };
+  },
+  methods: {
+    get
+  }
+};
 </script>
