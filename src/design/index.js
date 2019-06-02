088643757633e2e132bdb309b90480539a36e41e
MrChang <78172670@qq.com>
2019-06-03 02:03:22 +0800

    使用design组件

--- src/design/index.js
+++ src/design/index.js
@@ -1,5 +1,9 @@
 import 'ant-design-vue/dist/antd.css'
-import './styles'
+import './styles/index.scss'
 import Design from './Design.vue'
+import stripUUID from './stripUUID.js'
+import { createGroup } from './utils/component-group'
 
+Design.stripUUID = stripUUID;
+Design.group = createGroup
 export  default Design
--- src/views/AddPage.vue
+++ src/views/AddPage.vue
@@ -2,8 +2,10 @@
   <div>新增微页面</div>
 </template>
 <script>
+import Design from '../design/index'
 import ConfigEditor from "../components/config/ConfigEditor";
 import configConf from "../components/config/index";
+
 
 const components = [
   Object.assign({}, configConf, {
@@ -17,6 +19,20 @@ const components = [
     highlightWhenSelect: false
   })
 ];
+
+const groupedComponents = [
+  Object.assign({}, configConf,{
+    // 是否可以拖拽
+    dragable: false,
+    // 是否出现在底部的添加组件区域
+    appendable: false,
+    // 是否可以编辑，UMP里面有些地方config是不能编辑的
+    // editable: true,
+    configurable: false,
+    highlightWhenSelect: false
+  })
+  
+]
 console.info(ConfigEditor)
 export default {
   components: {
